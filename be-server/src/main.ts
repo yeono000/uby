965de6e21832f915943671aae5d01ca02b11fb25
yeono000 <tata0430@hanyang.ac.kr>
2024-02-22 14:53:02 +0900

    cors

--- be-server/src/main.ts
+++ be-server/src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.enableCors({
+    origin: true,//여기에 url을 넣어도된다. 
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Example API')
     .setDescription('The example API description')
